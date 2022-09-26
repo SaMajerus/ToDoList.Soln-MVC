68243f04917f7c5adaf1eef2342dd72f09fed4f8
Samuel Majerus <ladolego@gmail.com>
2022-09-26 16:31:26 -0700

    Adds implemented MVC Web Interface and associated project files.  [End of code-along work for Lsn 21]

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,49 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
   public class Program
   {
-    
-    public static void Main()
+    public static void Main(string[] args)
     {
-      welcome(); 
-      
-      int loop = 0;
-      bool repeatLoop = true;
-      while (repeatLoop)
-      {
-        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View) or end? (End)");
-        string userInput = Console.ReadLine().ToLower();
-        
-        if (userInput == "add") {
-          Console.WriteLine("Please enter the description for the new item.");
-          string item = Console.ReadLine();
-          Item newItem = new Item(item); 
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-        } else if (userInput == "view"){
-          Console.WriteLine("Your list: ");
-          List<Item> currentList = Item.GetAll();
-
-          foreach(Item i in currentList) 
-          {
-            loop += 1;
-            Console.WriteLine(loop + ") " + i.Description + \n);
-          }
-
-        } else if (userInput == "end"){
-          repeatLoop = false;
-
-        } else {
-          Console.WriteLine("This is not a valid command, please try again");
-        }
-      }
-
-      void welcome(){
-        Console.WriteLine("Welcome to the To Do List");       
-      }
+      host.Run();
     }
   }
 }
